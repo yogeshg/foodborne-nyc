e5abaf6c4a9e29e2728673c60ca80cc09da0176b
Yogesh Garg (Clic) <yg2482@columbia.edu>
2017-03-08 01:29:38 -0500

    running experiments

--- models.py
+++ models.py
@@ -92,9 +92,10 @@ def save_history(history, dirpath):
 
     return
 
-def main(full_data=False, finetune=False, filter_lengths = None, nb_filter = None):
+def load_data(full_data=False):
+    global embeddings_matrix, X_train, y_train, X_validate, y_validate, X_test, y_test
 
-    if(full_data):
+    if (full_data):
         datapath = '/tmp/yo/foodborne/yelp_labelled.csv'
         indexpath = '/tmp/yo/foodborne/vocab_yelp.txt'
         embeddingspath = '/tmp/yo/foodborne/vectors_yelp.txt'
@@ -103,23 +104,29 @@ def main(full_data=False, finetune=False, filter_lengths = None, nb_filter = Non
         indexpath = '/tmp/yo/foodborne/vocab_yelp_sample.txt'
         embeddingspath = '/tmp/yo/foodborne/vectors_yelp_sample.txt'
 
+
     embeddings_matrix = yelp.load_embeddings_matrix(indexpath, embeddingspath)
-    ((X, y), (X_test, y_test), _) = yelp.load_devset_testset_index(datapath, indexpath)
+    ((X, y), (X_test, y_test), _) = yelp.load_devset_testset_index(datapath, indexpath, ratio_dev_test=0.8)
     ratio_train_validate = 0.8
     ((X_train, y_train), (X_validate, y_validate)) = yelp.cutXY((X, y), ratio_train_validate)
 
     for x in (X_train, y_train, X_validate, y_validate, X_test, y_test):
         logger.debug("shape and info: "+str((x.shape, x.max(), x.min())))
 
-    maxlen = X.shape[1]
+
+
+def run_experiments(finetune=False, filter_lengths = None, nb_filter = None):
+    global embeddings_matrix, X_train, y_train, X_validate, y_validate, X_test, y_test
+
+    maxlen = X_train.shape[1]
     (vocab_size, dimensions) = embeddings_matrix.shape
     model, params = get_model(
         maxlen=maxlen, dimensions=dimensions, finetune=finetune, vocab_size=vocab_size,
         filter_lengths = filter_lengths, nb_filter = nb_filter, weights=[embeddings_matrix])
 
     results_dir = '/tmp/yo/foodborne/results/test/'
-    # with get_archiver(datadir='/tmp/yo/foodborne/results') as temp, get_archiver() as a:
-    with get_archiver() as a:
+    with get_archiver(datadir='/tmp/yo/foodborne/results') as temp, get_archiver() as a:
+    # with get_archiver() as a:
 
         with open(a.getFilePath('hyperparameters.json'), 'w') as f:
             json.dump(params, f, indent=2)
@@ -134,23 +141,29 @@ def main(full_data=False, finetune=False, filter_lengths = None, nb_filter = Non
 
         plot(model, to_file=a.getFilePath('model.png'), show_shapes=True, show_layer_names=True)
 
-        # modelpath = temp.getFilePath('weights.hdf5')
-        # callbacks = [
-        #     EarlyStopping(monitor='val_acc', patience=200, verbose=0),
-        #     ModelCheckpoint(modelpath, monitor='val_acc',
-        #         save_best_only=True, verbose=0),
-        # ]
-        # h = model.fit(X_train, y_train, batch_size=128, nb_epoch=2000,
-        #     validation_data=(X_validate, y_validate), callbacks=callbacks)
+        modelpath = temp.getFilePath('weights.hdf5')
+        callbacks = [
+            EarlyStopping(monitor='val_acc', patience=500, verbose=0),
+            ModelCheckpoint(modelpath, monitor='val_acc',
+                save_best_only=True, verbose=0),
+        ]
+        h = model.fit(X_train, y_train, batch_size=256, nb_epoch=3000,
+            validation_data=(X_validate, y_validate), callbacks=callbacks)
 
-        # save_history(h, a.getDirPath())
+        save_history(h, a.getDirPath())
 
     return
 
 if __name__ == '__main__':
-    for finetune in (False. True):
+    experiment_id = 0
+    experiments_to_run = map(int, sys.argv[1:])
+    load_data(full_data=True)
+    for finetune in (False, True):
         for nb_filter in (50,100,200):
             for filter_lengths_size in range(4):
                 filter_lengths = tuple((x+1 for x in range(filter_lengths_size)))
-                main(full_data=False, finetune=finetune, filter_lengths = filter_lengths, nb_filter = nb_filter)
+                if(experiment_id in experiments_to_run):
+                    logging.info('running experiment_id: {}'.format(experiment_id))
+                    run_experiments(finetune=finetune, filter_lengths = filter_lengths, nb_filter = nb_filter)
+                experiment_id += 1
     pass
