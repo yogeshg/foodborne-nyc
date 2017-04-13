17ea217831127bfffa52cc5b0395cf88df65b0a2
Yogesh Garg (Clic) <yg2482@columbia.edu>
2017-04-13 08:39:13 -0400

    auto commit staged files

--- run1.py
+++ run1.py
@@ -6,11 +6,11 @@ def main():
     embeddingspath  = '/tmp/yo/foodborne/vectors_yelp.txt'
     load_data(datapath, indexpath, embeddingspath, testdata=False)
     other_params = {'embeddingspath':embeddingspath}
-    run_experiments(finetune=False, kernel_sizes=(1,2,3), filters=5, lr=1e-3, pooling='logsumexp', kernel_l2_regularization=0.001, other_params=other_params)
-    run_experiments(finetune=False, kernel_sizes=(1,2,3,4), filters=5, lr=1e-3, pooling='logsumexp', kernel_l2_regularization=0.001, other_params=other_params)
+    run_experiments(finetune=False, kernel_sizes=(1,2,3), filters=50, lr=1e-3, pooling='logsumexp', kernel_l2_regularization=0.001, other_params=other_params)
+    run_experiments(finetune=False, kernel_sizes=(1,2,3,4), filters=50, lr=1e-3, pooling='logsumexp', kernel_l2_regularization=0.001, other_params=other_params)
 
-    run_experiments(finetune=False, kernel_sizes=(1,2,3), filters=50, lr=1e-3, pooling='max', kernel_l2_regularization=0.001, other_params=other_params)
-    run_experiments(finetune=False, kernel_sizes=(1,2,3,4), filters=50, lr=1e-3, pooling='max', kernel_l2_regularization=0.001, other_params=other_params)
+    # run_experiments(finetune=False, kernel_sizes=(1,2,3), filters=50, lr=1e-3, pooling='max', kernel_l2_regularization=0.001, other_params=other_params)
+    # run_experiments(finetune=False, kernel_sizes=(1,2,3,4), filters=50, lr=1e-3, pooling='max', kernel_l2_regularization=0.001, other_params=other_params)
 
 if __name__ == '__main__':
     main()
