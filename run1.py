cd869a0e8713eb2dd719f921f9b6b6691e9a2b98
Yogesh Garg (Clic) <yg2482@columbia.edu>
2017-05-04 17:01:55 -0400

    auto commit staged files

--- run1.py
+++ run1.py
@@ -10,7 +10,6 @@ main.run_experiments(finetune=False, kernel_sizes=(1,2,3,4),
                      filters=50, lr=1e-3, pooling='logsumexp',
                      kernel_l2_regularization=0.001, other_params=other_params)
 
-
 dataset = 'yelp'
 indexpath = '/tmp/yo/foodborne/vocab_yelp.txt'
 embeddingspath  = '/tmp/yo/foodborne/vectors_yelp.txt'
